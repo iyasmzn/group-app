6ae2a2b3c542ae887ce394e71e588ee579d1aeec
iyasmzn <iyasmzn07@gmail.com>
2025-09-22 13:29:54 +0700

    `Update useRealtimeTable hook to include type parameter for RealtimePostgresChangesFilter`

--- src/lib/hooks/useRealtimeTable.ts
+++ src/lib/hooks/useRealtimeTable.ts
@@ -37,7 +37,7 @@ export function useRealtimeTable<T>({
     const channel: RealtimeChannel = supabase.channel(`${table}-changes`)
 
     events.forEach((event) => {
-      const opts: RealtimePostgresChangesFilter = {
+      const opts: RealtimePostgresChangesFilter<Record<string, unknown>> = {
         event,
         schema,
         table,
